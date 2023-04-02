2c97a3d5062117d7743af53b8d102b7cd7db18b7
Yash Datta <yd2590@columbia.edu>
2023-04-02 23:21:12 +0800

    Try

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://saucam.github.io/',
+  url: 'https://saucam.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/shiva/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
